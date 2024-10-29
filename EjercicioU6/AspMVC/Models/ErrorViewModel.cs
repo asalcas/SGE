bbc5efacc2dbeebd02a66d68978169aa4eec9ddd
asalcas <alvaro.salvador@iesnervion.es>
2024-10-29 14:15:51 +0100

    Delete EjercicioU6 directory

--- EjercicioU6/AspMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
