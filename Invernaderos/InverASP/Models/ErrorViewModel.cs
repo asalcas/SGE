7f68fa9615a62ad2fc877c110163fab8ec850f8c
asalcas <alvaro.salvador@iesnervion.es>
2025-05-14 00:22:16 +0200

    Carpeta que elimine desde VisualStudio no se eliminó.

--- Invernaderos/InverASP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InverASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
